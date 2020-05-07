e63098e4663ecf18c979fd432799af3a4dad4883
澁澤賢 <>
2020-05-07 15:03:15 +0900

    no message

--- jsdom.js
+++ jsdom.js
@@ -3,12 +3,12 @@ const {
   JSDOM
 } = require('jsdom')
 
-request('http://www.uec.ac.jp/', (e, response, body) => {
+request('http://www.uec.ac.jp/', (a, response, body) => {
   if (a) {
     console.error(a)
-  }
+}
 
-  try {
+try {
     const dom = new JSDOM(body)
     console.log(`${dom.toString()}`)
   } catch (e) {
